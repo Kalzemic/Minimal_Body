11a3075db7334c25262dedc198430eae5c6caab8
Kalzemic <Mizizov@gmail.com>
2025-04-12 18:29:56 +0300

    fixed sub page

--- src/components/ContactPage.tsx
+++ src/components/ContactPage.tsx
@@ -66,34 +66,37 @@ const ContactPage = () => {
 
 
     return (
-        <div className="image-container">
-            <BackgroundImage url="minimal_body_3.png">
-                <div className="page-content">
-                    <h1>📲 רוצים לשמוע עוד? צרו איתנו קשר עוד היום!
-                    </h1>
-                    <form className="contact-form" onSubmit={handleSubmit} >
-                        <div className="segment">
-                            <label>שם:</label>
-                            <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
-                        </div>
-                        <div className="segment">
-                            <label>דואר אלקטרוני:</label>
-                            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-                        </div>
-                        <div className="segment">
-                            <label>מספר טלפון:</label>
-                            <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)}></input>
-                        </div>
-                        <div className="segment">
-                            <button type="submit" disabled={cooldown}>
-                                {cooldown ? `חכו ${secondsLeft} שניות...` : 'לחצו להרשמה'}
-                            </button>
+        <div className="mainpage">
+            <div className="image-container">
+                <BackgroundImage url="minimal_body_3.png">
+                    <div className="page-content">
+                        <h1>📲 רוצים לשמוע עוד? צרו איתנו קשר עוד היום!
+                        </h1>
+                        <form className="contact-form" onSubmit={handleSubmit} >
+                            <div className="segment">
+                                <label>שם:</label>
+                                <input type="text" value={name} onChange={(e) => setName(e.target.value)}></input>
+                            </div>
+                            <div className="segment">
+                                <label>דואר אלקטרוני:</label>
+                                <input type="text" value={email} onChange={(e) => setEmail(e.target.value)}></input>
+                            </div>
+                            <div className="segment">
+                                <label>מספר טלפון:</label>
+                                <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)}></input>
+                            </div>
+                            <div className="segment">
+                                <button type="submit" disabled={cooldown}>
+                                    {cooldown ? `חכו ${secondsLeft} שניות...` : 'לחצו להרשמה'}
+                                </button>
 
-                        </div>
-                    </form>
-                </div>
-            </BackgroundImage>
+                            </div>
+                        </form>
+                    </div>
+                </BackgroundImage>
+            </div>
         </div>
+
     );
 }
 
--- src/components/Subscriptions.tsx
+++ src/components/Subscriptions.tsx
@@ -4,35 +4,37 @@ import './Subscriptions.css'
 
 const Subscriptions: React.FC = () => {
     return (
+
         <div className="image-container">
             <BackgroundImage url="/minimal_body_2.png">
-                <div className="page-content">
+                <div className="sub-page">
                     <h1>המנויים שלנו</h1>
-                    <div className="cards-grid">
-                        <div className="sub-card">
-                            <h1>Dual Access</h1>
-                            <h2>מנוי לשני אימונים שבועיים</h2>
-                            <p>379 ש"ח</p>
-                        </div>
-                        <div className="sub-card">
-                            <h1>Pro</h1>
-                            <h2>3 אימונים שבועיים</h2>
-                            <p>439 ש"ח</p>
-                        </div>
-                        <div className="sub-card">
-                            <h1>Unlimited</h1>
-                            <h2>מנוי ללא הגבלת כניסה</h2>
-                            <p>599 ש"ח</p>
-                        </div>
-                        <div className="sub-card">
-                            <h1>NEXTGEN</h1>
-                            <h2>מנוי לשני אימונים שבועיים עבור בני נוער</h2>
-                            <p>379 ש"ח</p>
-                        </div>
+
+                    <div className="sub-card">
+                        <h1>Dual Access</h1>
+                        <h2>מנוי לשני אימונים שבועיים</h2>
+                        <p>379 ש"ח</p>
                     </div>
+                    <div className="sub-card">
+                        <h1>Pro</h1>
+                        <h2>3 אימונים שבועיים</h2>
+                        <p>439 ש"ח</p>
+                    </div>
+                    <div className="sub-card">
+                        <h1>Unlimited</h1>
+                        <h2>מנוי ללא הגבלת כניסה</h2>
+                        <p>599 ש"ח</p>
+                    </div>
+                    <div className="sub-card">
+                        <h1>NEXTGEN</h1>
+                        <h2>מנוי לשני אימונים שבועיים עבור בני נוער</h2>
+                        <p>379 ש"ח</p>
+                    </div>
+
                 </div>
             </BackgroundImage>
         </div>
+
     );
 }
 
