a8d58c1a815b0bc61d1dfb246a9903edaa74bd82
Kalzemic <Mizizov@gmail.com>
2025-05-11 10:54:13 +0300

    changes to add contact and sub pages to main homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Navbar from './components/Navbar'
+// import Navbar from './components/Navbar'
 import HomePage from './components/HomePage'
 import { FaInstagram, FaWhatsapp } from 'react-icons/fa';
-import ContactPage from './components/ContactPage'
 import Subscriptions from './components/Subscriptions'
 
 function App() {
@@ -14,12 +13,10 @@ function App() {
 
   return (
     <BrowserRouter>
-      <Navbar />
+      {/* <Navbar /> */}
       <div className="main-container">
         <Routes>
           <Route path="/" element={<HomePage />} />
-
-          <Route path="/Contact" element={<ContactPage />} />
           <Route path="/Subscriptions" element={<Subscriptions />} />
         </Routes>
       </div>
