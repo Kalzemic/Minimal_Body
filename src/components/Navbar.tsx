12683ea5beba81942fa3c20f216ab9a79516c24d
Kalzemic <Mizizov@gmail.com>
2025-06-08 19:55:02 +0300

    side bar options

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,48 @@
-
 import './Navbar.css'
+import { useState } from 'react';
 
 function Navbar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleSidebar = () => setIsOpen(!isOpen);
+
     return (
-        <nav className="navbar">
-            <div className="navbar-content">
-                <div className="navbar-left">
-                    <img src="minimal_body_logo.png" alt="Company Logo" className="logo" />
+        <>
+            <nav className="navbar">
+                <div className="navbar-content">
+                    <div className="navbar-left">
+                        <img src="minimal_body_logo.png" alt="Company Logo" className="logo" />
+                    </div>
+
+                    {/* Desktop nav */}
+                    <div className="navbar-center desktop-menu">
+                        <ul>
+                            <li><a href="#home">דף הבית</a></li>
+                            <li><a href="#signup">להרשמה</a></li>
+                            <li><a href="#subscriptions">אפשרויות מנוי</a></li>
+                            <li><a href="#why_us">למה Minimal Body?</a></li>
+                            <li><a href="#method">השיטה שלנו</a></li>
+                        </ul>
+                    </div>
+
+                    {/* Mobile menu button */}
+                    <div className="hamburger-menu" onClick={toggleSidebar}>
+                        ☰
+                    </div>
                 </div>
-                <div className="navbar-center">
-                    <ul>
-                        <li><a href="#home">דף הבית</a></li>
-                        <li><a href="#signup">להרשמה</a></li>
-                        <li><a href="#subscriptions">אפשרויות מנוי</a></li>
-                        <li> <a href="#why_us">למה Minimal Body?</a></li>
-                        <li><a href="#method">השיטה שלנו</a></li>
-                    </ul>
-                </div>
-                <div className="navbar-right" />
+            </nav>
+
+            {/* Sidebar menu */}
+            <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+                <ul>
+                    <li><a href="#home" onClick={toggleSidebar}>דף הבית</a></li>
+                    <li><a href="#signup" onClick={toggleSidebar}>להרשמה</a></li>
+                    <li><a href="#subscriptions" onClick={toggleSidebar}>אפשרויות מנוי</a></li>
+                    <li><a href="#why_us" onClick={toggleSidebar}>למה Minimal Body?</a></li>
+                    <li><a href="#method" onClick={toggleSidebar}>השיטה שלנו</a></li>
+                </ul>
             </div>
-        </nav>
+        </>
     );
 }
 
